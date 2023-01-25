a667b391d84da72f6921eb271ef61aa608818f04
DASitby <davidsooley551@gmail.com>
2023-01-25 20:24:00 +0000

    write renderTweets function and call it using the fake preloaded example tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,11 +3,11 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
-const createTweetElement = (tweetData) => {
-  const user = tweetData.user;
-  const content = tweetData.content;
-  const $tweet = (`<article>
+$(document).ready(function() {
+  const createTweetElement = (tweetData) => {
+    const user = tweetData.user;
+    const content = tweetData.content;
+    const $tweet = (`<article>
   <header id="tweet-header">
     <div id="tweet-name">
       ${user.name}
@@ -30,23 +30,45 @@ const createTweetElement = (tweetData) => {
     </div>
   </footer>
 </article>`);
-  return $tweet;
-};
+    return $tweet;
+  };
 
-// Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-};
+  const renderTweets = function(tweets) {
+    let $tweetsContainer = $("#tweets-container");
+    // loops through tweets
+    tweets.forEach(tweet => {
+    // calls createTweetElement for each tweet
+      let element = createTweetElement(tweet);
+      // takes return value and appends it to the tweets container
+      $tweetsContainer.append(element);
+    });
+  };
 
-const $tweet = createTweetElement(tweetData);
+  // Fake data taken from initial-tweets.json
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
+  renderTweets(data);
+});
