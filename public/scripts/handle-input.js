3a7fb5a6b2230c7245dc75ee7aa4a933cf78ca05
DASitby <davidsooley551@gmail.com>
2023-01-25 23:55:16 +0000

    add error handling it input handler for empty or too long inputs

--- public/scripts/handle-input.js
+++ public/scripts/handle-input.js
@@ -5,8 +5,14 @@ $(document).ready(function() {
 
   $form.submit((event) => {
     event.preventDefault();
+    if ($input.val() === '' || null) {
+      return alert("Please enter at least one character in your Tweet");
+    }
+    if ($input.val().length > 140) {
+      return alert("Your tweet is too long");
+    }
+    console.log($input.val());
     const tweetQuery = $input.serialize();
-    console.log(tweetQuery);
     $.post("/tweets", tweetQuery);
   });
 });
