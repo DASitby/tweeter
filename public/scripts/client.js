e78145e187dadd9f02b01752dc0de639b8be22af
DASitby <davidsooley551@gmail.com>
2023-01-28 18:50:18 +0000

    add a hidden error element which is given relevent text and made visible when a tweet error occurs, replacing old alerts

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -67,10 +67,14 @@ $(document).ready(function() {
   $form.submit((event) => {
     event.preventDefault();
     if ($input.val() === '' || null) {
-      return alert("Please enter at least one character in your Tweet");
+      $('#error').removeAttr('hidden');
+      $('#error').text("Please enter at least one character in your Tweet");
+      return;
     }
     if ($input.val().length > 140) {
-      return alert("Your tweet is too long");
+      $('#error').removeAttr('hidden');
+      $('#error').text("Your tweet is too long");
+      return;
     }
     const tweetQuery = $input.serialize();
     $.post("/tweets", tweetQuery, () => {
