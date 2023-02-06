43a4a8868441ad8c9fb4f864df7f26c84c7b5aee
DASitby <davidsooley551@gmail.com>
2023-02-06 12:02:26 +0000

    move function declarations outside of client.js document.ready call

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,56 +1,56 @@
 /* eslint-disable no-undef */
-$(document).ready(function() {
-  ////////////////////////
-  ///FUNCTION DECLARATIONS
-  ////////////////////////
-  const createTweetElement = (tweetData) => {
-    const user = tweetData.user;
-    const content = tweetData.content;
-    const escape = function(str) {
-      let div = document.createElement("div");
-      div.setAttribute("id", "tweet-body");
-      div.appendChild(document.createTextNode(str));
-      return div.innerHTML;
-    };
-    const safeHTML = `<p>${escape(content.text)}</p>`;
-    const $tweet = (`<article>
-    <header id="tweet-header">
-    <div id="tweet-name">
-      <img class="avatar" src=${user.avatars}>${user.name}
-    </div>
-    <div id="tweet-user">
-    ${user.handle}
-    </div>
-  </header>
-  ${safeHTML}
+////////////////////////
+///FUNCTION DECLARATIONS
+////////////////////////
+const createTweetElement = (tweetData) => {
+  const user = tweetData.user;
+  const content = tweetData.content;
+  const escape = function(str) {
+    let div = document.createElement("div");
+    div.setAttribute("id", "tweet-body");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+  const safeHTML = `<p>${escape(content.text)}</p>`;
+  const $tweet = (`<article>
+  <header id="tweet-header">
+  <div id="tweet-name">
+    <img class="avatar" src=${user.avatars}>${user.name}
   </div>
-  <footer id="tweet-footer">
-    <div id="tweet-age">
-    ${timeago.format(tweetData.created_at)}
-    </div> 
-    <div id="tweet-icons">
-      <i class="fa-solid fa-flag tweet-icon"></i>
-      <i class="fa-solid fa-retweet tweet-icon"></i>
-      <i class="fa-solid fa-heart tweet-icon"></i>
-    </div>
-  </footer>
+  <div id="tweet-user">
+  ${user.handle}
+  </div>
+</header>
+${safeHTML}
+</div>
+<footer id="tweet-footer">
+  <div id="tweet-age">
+  ${timeago.format(tweetData.created_at)}
+  </div> 
+  <div id="tweet-icons">
+    <i class="fa-solid fa-flag tweet-icon"></i>
+    <i class="fa-solid fa-retweet tweet-icon"></i>
+    <i class="fa-solid fa-heart tweet-icon"></i>
+  </div>
+</footer>
 </article>`);
-    return $tweet;
-  };
-  const renderTweets = function(tweets) {
-    for (let i = tweets.length - 1; i >= 0; i--) {
-      let tweet = tweets[i];
-      // calls createTweetElement for each tweet and appends it to the tweets container
-      let element = createTweetElement(tweet);
-      $("#tweets-container").append(element);
+  return $tweet;
+};
+const renderTweets = function(tweets) {
+  for (let i = tweets.length - 1; i >= 0; i--) {
+    let tweet = tweets[i];
+    // calls createTweetElement for each tweet and appends it to the tweets container
+    let element = createTweetElement(tweet);
+    $("#tweets-container").append(element);
 
-    }
-  };
-  const loadTweets = () => {
-    $.get("/tweets", (newTweets) => {
-      renderTweets(newTweets);
-    });
-  };
+  }
+};
+const loadTweets = () => {
+  $.get("/tweets", (newTweets) => {
+    renderTweets(newTweets);
+  });
+};
+$(document).ready(function() {
   ///////////////
   //FORM HANDLING
   ///////////////
